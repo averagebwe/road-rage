f962eb7cf7de8790e031babcba9e0baf91d59e8f
averagebwe <ivannaiv1337@gmail.com>
2024-03-15 01:23:17 +0600

    Health/death system

--- xd.py
+++ xd.py
@@ -31,6 +31,8 @@ char = [load_image('player1.png'), load_image(
 
 clock = pygame.time.Clock()
 
+score = 0
+
 
 class Player(object):
     def __init__(self, x, y, width, height):
@@ -40,12 +42,28 @@ class Player(object):
         self.height = height
         self.speed = 7
         self.rideCount = 0
+        self.hitbox = (self.x, self.y, 30, 30)
+        self.health = 15
+        self.visible = True
 
     def draw(self, screen):
-        if self.rideCount + 1 >= 15:
-            self.rideCount = 0
-        screen.blit(char[self.rideCount // 5], (self.x, self.y))
-        self.rideCount += 1
+        if self.visible:
+            if self.rideCount + 1 >= 15:
+                self.rideCount = 0
+            screen.blit(char[self.rideCount // 5], (self.x, self.y))
+            self.rideCount += 1
+            self.hitbox = (self.x, self.y, 30, 30)
+            pygame.draw.rect(screen, (255, 0, 0),
+                             (self.hitbox[0], self.hitbox[1] - 20, 30, 5))
+            pygame.draw.rect(screen, (0, 255, 0),
+                             (self.hitbox[0], self.hitbox[1] - 20, 30 - ((30 / 15) * (15 - self.health)), 5))
+        # pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
+
+    def hit(self):
+        if self.health > 0:
+            self.health -= 1
+        else:
+            self.visible = False
 
 
 class Enemy(object):
@@ -62,6 +80,8 @@ class Enemy(object):
         self.rideCount = 0
         self.speed = speed
         self.velocity = velocity
+        self.hitbox = (self.x, self.y, 50, 50)
+        self.health = 8
 
     def draw(self, screen):
         self.move()
@@ -70,6 +90,13 @@ class Enemy(object):
             self.rideCount = 0
         screen.blit(self.en[self.rideCount // 3], (self.x, self.y))
         self.rideCount += 1
+        self.hitbox = (self.x, self.y, 50, 50)
+
+        pygame.draw.rect(screen, (255, 0, 0),
+                         (self.hitbox[0], self.hitbox[1] - 20, 50, 5))
+        pygame.draw.rect(screen, (0, 255, 0),
+                         (self.hitbox[0], self.hitbox[1] - 20, 50 - ((50 / 8) * (8 - self.health)), 5))
+        # pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
 
     def move(self):
         if self.y + self.speed < self.path[1]:
@@ -80,8 +107,16 @@ class Enemy(object):
     def shoot(self):
         if self.speed == 0:
             if random.randrange(0, self.velocity * FPS) == 1:
-                bullets.append(Projectile(
+                enemy_bullets.append(Projectile(
                     round(self.x + self.width // 2), round(self.y + self.height), 3, (255, 255, 225), 14, 1))
+
+    def hit(self):
+        global score
+        if self.health > 0:
+            self.health -= 1
+        else:
+            enemies.pop(enemies.index(self))
+            score += 1
 
 
 class Projectile(object):
@@ -99,17 +134,23 @@ class Projectile(object):
 
 def redrawGameWindow():
     screen.blit(background, (0, 0))
+    text = font.render('УНИЧТОЖЕНО: ' + str(score), 1, (255, 255, 255))
+    screen.blit(text, (10, 10))
     player.draw(screen)
     for enemy in enemies:
         enemy.draw(screen)
     for bullet in bullets:
         bullet.draw(screen)
+    for bullet in enemy_bullets:
+        bullet.draw(screen)
 
     pygame.display.update()
 
 
-player = Player(100, 100, 30, 30)
+font = pygame.font.Font('data/3712-font.otf', 30)
+player = Player(WINDOW_WIDTH // 2, 900, 30, 30)
 bullets = []
+enemy_bullets = []
 enemies = [Enemy(random.randrange(0, WINDOW_WIDTH - 50), 0, 50,
                  50, 4, random.randrange(50, WINDOW_HEIGHT - 500), 3) for _ in range(random.randrange(3, 11))]
 prev_time = pygame.time.get_ticks()
@@ -122,7 +163,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    if enemies == []:
+        enemies = [Enemy(random.randrange(0, WINDOW_WIDTH - 50), 0, 50,
+                         50, 4, random.randrange(50, WINDOW_HEIGHT - 500), 3) for _ in range(random.randrange(3, 11))]
+
     for bullet in bullets:
+        for enemy in enemies:
+            if bullet.y - bullet.radius < enemy.hitbox[1] + enemy.hitbox[3] and bullet.y + bullet.radius > enemy.hitbox[1]:
+                if bullet.x + bullet.radius > enemy.hitbox[0] and bullet.x - bullet.radius < enemy.hitbox[0] + enemy.hitbox[2]:
+                    enemy.hit()
+                    bullets.pop(bullets.index(bullet))
+
         if bullet.x < 1280 and bullet.x > 0:
             pass
         else:
@@ -132,26 +183,42 @@ while running:
         else:
             bullets.pop(bullets.index(bullet))
 
+    for enemy_bullet in enemy_bullets:
+        if enemy_bullet.y - enemy_bullet.radius < player.hitbox[1] + player.hitbox[3] and enemy_bullet.y + enemy_bullet.radius > player.hitbox[1]:
+            if enemy_bullet.x + enemy_bullet.radius > player.hitbox[0] and enemy_bullet.x - enemy_bullet.radius < player.hitbox[0] + player.hitbox[2]:
+                player.hit()
+                enemy_bullets.pop(enemy_bullets.index(enemy_bullet))
+
+        if enemy_bullet.x < 1280 and enemy_bullet.x > 0:
+            pass
+        else:
+            enemy_bullets.pop(enemy_bullets.index(enemy_bullet))
+        if enemy_bullet.y < 960 and enemy_bullet.y > 0:
+            enemy_bullet.y += enemy_bullet.speed
+        else:
+            enemy_bullets.pop(enemy_bullets.index(enemy_bullet))
+
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_SPACE]:
-        current_time = pygame.time.get_ticks()
-        # задержка стрельбы по времени
-        if current_time - prev_time > 200:
-            prev_time = current_time
-            bullets.append(Projectile(
-                round(player.x + player.width // 2), round(player.y), 3, (255, 255, 224), 18, -1))
+        if player.visible == True:
+            current_time = pygame.time.get_ticks()
+            # задержка стрельбы по времени
+            if current_time - prev_time > 200:
+                prev_time = current_time
+                bullets.append(Projectile(
+                    round(player.x + player.width // 2), round(player.y), 3, (255, 255, 224), 18, -1))
 
-    if keys[pygame.K_LEFT] or keys[ord('a')]:
+    if keys[pygame.K_LEFT] or keys[ord('a')] and player.x > player.speed:
         player.x -= player.speed
 
-    if keys[pygame.K_RIGHT] or keys[ord('d')]:
+    if keys[pygame.K_RIGHT] or keys[ord('d')] and player.x < WINDOW_WIDTH - player.width - player.speed:
         player.x += player.speed
 
-    if keys[pygame.K_UP] or keys[ord('w')]:
+    if keys[pygame.K_UP] or keys[ord('w')] and player.y > player.speed:
         player.y -= player.speed
 
-    if keys[pygame.K_DOWN] or keys[ord('s')]:
+    if keys[pygame.K_DOWN] or keys[ord('s')] and player.y < WINDOW_HEIGHT - player.height - player.speed:
         player.y += player.speed
 
     redrawGameWindow()
